fc9f168d0c3d181e02e93a243fe214ee7424fdb5
Viktoria Zharina <159477668+ViktoriaZharina@users.noreply.github.com>
2024-05-23 18:06:12 +0300

    task-1

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,15 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItems = images.map(({ url, alt }) => {
+  return `
+    <li>
+      <img src="${url}" alt="${alt}">
+    </li>
+  `;
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryItems);
